aadb15eb712ed20200186f3fedcbac5486367a14
xavier <kingdom_xiv@hotmail.com>
2020-12-24 14:46:07 +0800

    add chapter01 example

---
+++ Chapter01.Syntax-Basics/example.05.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	// var quantity int = 6
+	// var length, width float64 = 1.2, 2.4
+	// var customerName string = "Damon Cole"
+
+	// var quantity = 6
+	// var length, width = 1.2, 2.4
+	// var customerName = "Damon Cole"
+
+	// quantity = 4
+	// length, width = 1.2, 2.4
+	// customerName = "Damon Cole"
+
+	quantity := 4
+	length, width := 1.2, 2.4
+	customerName := "Damon Cole"
+
+	fmt.Println(customerName)
+	fmt.Println("has ordered", quantity, "sheets")
+	fmt.Println("each with an area of")
+	fmt.Println(length * width, "square meters")
+}
